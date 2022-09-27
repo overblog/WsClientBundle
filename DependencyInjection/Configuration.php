a9cca8e1a2c8a5e063a11158586cfe69c4db8640
Jeremiah VALERIE <mcg-web@users.noreply.github.com>
2022-09-27 10:55:30 +0200

    Support Symfony 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('overblog_ws_client');
+        $treeBuilder = new TreeBuilder('overblog_ws_client');
+        $rootNode = \method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('overblog_ws_client');
 
         $rootNode
             ->children()
